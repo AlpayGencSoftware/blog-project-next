f13a2cb0ab265a119d1060a7dd3208dcd8c3c11f
AlpayGencSoftware <gencoalpay@gmail.com>
2023-09-22 13:00:28 +0300

    On demand revalidation in Next

--- lib/getFormattedDate.tsx
+++ lib/getFormattedDate.tsx
@@ -1,3 +1,10 @@
-export default function getFormattedDate(dateString:string):string{
-    return new Intl.DateTimeFormat('en-US', {dateStyle:'long'}).format(new Date(dateString))
+export default function getFormattedDate(dateString: string): string {
+    const date = new Date(dateString);
+    
+    if (isNaN(date.getTime())) {
+        console.error("Geçersiz tarih:", dateString);
+        return "Geçersiz Tarih"; // veya herhangi bir hata mesajı ya da boş bir string döndürebilirsiniz.
+    }
+
+    return new Intl.DateTimeFormat('tr-TR', { dateStyle: 'long' }).format(date);
 }
